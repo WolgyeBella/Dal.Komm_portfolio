3346084d3f039b35d84c54f99bac801d6e25133b
WolgyeBella <gkdud9209@gmail.com>
2022-05-18 20:38:20 +0900

    section colorful html, css complete

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// let roller = document.querySelector('.menu');
+// roller.id = 'roller1';
+
+// let clone = roller.cloneNode(true);
+// clone.id = 'roller2';
+// document.querySelector('.img-moving').appendChild(clone);
+
+// document.querySelector('#roller1').style.left = '0px';
+// document.querySelector('#roller2').style.left = document.querySelector('.menu').offsetWidth + 'px';
+
+// roller.classList.add('original');
+// roller.classList.add('clone');
